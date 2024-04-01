730be844fd836c4b482191d6e8d4bdb4e45dd302
Oleksandr Basiuk <basuksasa@gmail.com>
2024-04-01 15:17:26 +0200

    add functional delete and create posts,create component postForm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import React, { useState } from "react";
-import Counter from "./components/Counter";
-import ClassCounter from "./components/classCounter";
-
+import React, { useRef, useState } from "react";
 import "./styles/App.css";
-import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -15,27 +10,22 @@ function App() {
         { id: 3, title: "Javascript 3", body: "Description" },
     ]);
 
-    const [title, setTitle] = useState("");
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost]);
+    };
 
-    const addNewPost = (e) => {
-        e.preventDefault();
-
-        console.log(title);
+    const removePost = (post) => {
+        setPosts(posts.filter((p) => p.id !== post.id));
     };
 
     return (
         <div className="App">
-            <form>
-                <MyInput
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    type="text"
-                    placeholder="Title post"
-                />
-                <MyInput type="text" placeholder="Description post" />
-                <MyButton onClick={addNewPost}>Create post</MyButton>
-            </form>
-            <PostList posts={posts} title="Посты JS" />
+            <PostForm create={createPost} />
+            {posts.length !== 0 ? (
+                <PostList remove={removePost} posts={posts} title="Posts JS" />
+            ) : (
+                <h1 style={{ textAlign: "center" }}>Posts not fiend</h1>
+            )}
         </div>
     );
 }
--- src/components/UI/input/MyInput.jsx
+++ src/components/UI/input/MyInput.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import classes from "./MyInput.module.css";
 
-const MyInput = (props) => {
-    return <input className={classes.MyInput} {...props} />;
-};
+const MyInput = React.forwardRef((props, ref) => {
+    return <input ref={ref} className={classes.MyInput} {...props} />;
+});
 
 export default MyInput;
