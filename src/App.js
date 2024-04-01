c9886d9e9c9d96f45139498ff3255595ee12f433
Oleksandr Basiuk <basuksasa@gmail.com>
2024-04-01 17:20:23 +0200

    add sort component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { useRef, useState } from "react";
 import "./styles/App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
     const [posts, setPosts] = useState([
-        { id: 1, title: "Javascript", body: "Description" },
-        { id: 2, title: "Javascript 2", body: "Description" },
-        { id: 3, title: "Javascript 3", body: "Description" },
+        { id: 1, title: "aa", body: "Description" },
+        { id: 2, title: "dd 2", body: "Description" },
+        { id: 3, title: "aa 3", body: "Description" },
     ]);
+
+    const [selectedSort, setSelectedSort] = useState("");
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
@@ -18,9 +21,26 @@ function App() {
         setPosts(posts.filter((p) => p.id !== post.id));
     };
 
+    const sortPosts = (sort) => {
+        console.log(sort);
+        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
+    };
+
     return (
         <div className="App">
             <PostForm create={createPost} />
+            <hr style={{ margin: "15px 0" }} />
+            <div>
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortPosts}
+                    defaultValue="Sort by"
+                    options={[
+                        { value: "title", name: "For name" },
+                        { value: "body", name: "For description" },
+                    ]}
+                />
+            </div>
             {posts.length !== 0 ? (
                 <PostList remove={removePost} posts={posts} title="Posts JS" />
             ) : (
